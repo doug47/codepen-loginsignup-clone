d42664d556ea22af471c8367b2b6374ee332f278
Colt Henrie <colt@Admins-MacBook-Pro.local>
2013-09-26 23:15:34 -0600

    this is my first push

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ codepen.api.signup(user_object)
 	parameters: user object, which contains properties: name, email, username, password
 	returns: response object
 
-codepen.login(user_object)
+codepen.api.login(user_object)
 	description: to login an existing user
 	parameters: user object, which contains properties: username, password
 	returns: response object
@@ -30,7 +30,61 @@ Reponse Objects:
 
 
 $('document').ready(function() {
+	var login_users = {
+		username:"", 
+		password:""
+	};
+	
+	$('.signup-form').addClass("hidden_form");
+	
 
+	$('#nav_signup_button').click(function() 
+	{
+		//Switching forms	
+			$('.signup-form').removeClass("hidden_form");
+			$('.login-form').addClass("hidden_form");
+
+		//Active Button Styling
+			$('#nav_login_button').removeClass("active");
+			$('#nav_signup_button').addClass("active");
+
+
+	});
+
+	$('#nav_login_button').click(function() 
+	{
+		//Switching forms
+		
+		$('.login-form').removeClass("hidden_form");
+		$('.signup-form').addClass("hidden_form");
+
+		//Active Button Styling
+		$('#nav_signup_button').removeClass("active");
+		$('#nav_login_button').addClass("active");
+
+
+	});
+
+	$('#login_button').click(function() 
+	{
+		login_users['username'] = $('#login-email-field').val();
+		login_users['password'] = $('#login-password-field').val();
+		console.log("Username:", login_users['username']);
+		console.log("Password:", login_users['password']);
+		var server_check = codepen.api.login(login_users);
+
+		if (server_check['success'] == true)
+		{
+			alert("You successfully logged into nothing!")
+		}
+		else
+		{
+			alert(server_check['error'])
+		}
+		return false;
+
+	})
+	
 	//put code here
 
 });
